c87d2296f85ea4174d44f18ed738d7d09c186820
Jiho <hoya4139@naver.com>
2025-03-26 02:05:39 +0900

    create backend project

---
+++ backend/lsal/src/main/java/com/skku/lsal/LsalApplication.java
@@ -0,0 +1,13 @@
+package com.skku.lsal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LsalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LsalApplication.class, args);
+	}
+
+}
---
+++ backend/lsal/src/test/java/com/skku/lsal/LsalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skku.lsal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LsalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
